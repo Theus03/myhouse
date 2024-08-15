9ca7e00d10af6bd969e17b071acb537ee2a9059c
Matheus Leite <matheusssleite@outlook.com>
2024-08-15 19:10:17 -0300

    Teste para publicação no github

---
+++ src/components/Menu.tsx
@@ -0,0 +1,5 @@
+export default function Menu() {
+    return (
+        <h1>Aqui é o menu</h1>
+    )   
+}
---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <main>
+            <img src="/logo.svg" alt="" />
+            <h1>ENTRE E CONHEÇA UM POUCO <br/> MAIS SOBRE NOSSA CORRETORA </h1>
+            <button>ENTRAR COM O GOOGLE</button>
+        </main>
+    )
+}
