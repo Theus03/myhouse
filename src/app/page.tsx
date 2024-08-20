03b6b3ab53c98ea5acd0053528aa282ccf19d23e
Matheus Leite <matheusssleite@outlook.com>
2024-08-20 19:17:03 -0300

    Adicionando estrutura HTML da sessão portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button"
 import Menu from "@/components/Menu"
+import Portfolio from "@/components/Portfolio"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <h2 className="text-slogan">FAREMOS DA SUA CASA,<br/> UM LUGAR PERFEITO<br/> PARA MORAR.</h2>
         <Button _type="outline" _textButton={"Entrar em Contato"}/>
       </div>
+      <Portfolio/>
     </main>
   )
 }
