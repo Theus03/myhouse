8d125c2fd133f40f34342c9465ff5e85d31d11df
Theus03 <matheusssleite@outlook.com>
2024-08-19 23:35:18 -0300

    Terminando restante do código do banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import Menu from "@/components/Menu"
 export default function Home() {
   return (
     <main>
+      <img src="banner.png" className="img-banner" alt="Imagem de Banner"/>
       <Menu/>
-      <div className="banner-slogan">
-        <img src="banner.png" className="img-banner" alt="Imagem de Banner"/>
-        <div className="container-slogan">
-          <h2 className="text-slogan">FAREMOS DA SUA CASA,<br/> UM LUGAR PERFEITO<br/> PARA MORAR.</h2>
-          <Button _type="outline" _textButton={"Entrar em Contato"}/>
-        </div>
+      <div className="container-slogan">
+        <h2 className="text-slogan">FAREMOS DA SUA CASA,<br/> UM LUGAR PERFEITO<br/> PARA MORAR.</h2>
+        <Button _type="outline" _textButton={"Entrar em Contato"}/>
       </div>
     </main>
   )
