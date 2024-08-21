791bfd86b6a746cf968cf40fa944d0d01eef0234
Theus03 <matheusssleite@outlook.com>
2024-08-20 22:24:28 -0300

    Adicionando estilo na sessão de portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import Portfolio from "@/components/Portfolio"
 export default function Home() {
   return (
     <main>
-      <img src="banner.png" className="img-banner" alt="Imagem de Banner"/>
       <Menu/>
-      <div className="container-slogan">
-        <h2 className="text-slogan">FAREMOS DA SUA CASA,<br/> UM LUGAR PERFEITO<br/> PARA MORAR.</h2>
-        <Button _type="outline" _textButton={"Entrar em Contato"}/>
-      </div>
+      <section className="container-banner">
+        <div className="container-slogan">
+          <h2 className="text-slogan">FAREMOS DA SUA CASA,<br/> UM LUGAR PERFEITO<br/> PARA MORAR.</h2>
+          <Button _type="outline" _textButton={"Entrar em Contato"}/>
+        </div>
+      </section>
       <Portfolio/>
     </main>
   )
