d9755010a015379c65b1f27b96320eb278c7862f
Theus03 <matheusssleite@outlook.com>
2024-08-22 08:06:29 -0300

    Ajustando responsividade do site

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -4,21 +4,21 @@ export default function Portfolio() {
             <div className="card-portfolio">
                 <img src="new-york.png" alt="Imagem de New York" />
                 <div className="container-text-portfolio">
-                    <img src="icon-map.png" alt="ícone de Localização" />
+                    <img className="icon-map" src="icon-map.png" alt="ícone de Localização" />
                     <h3 className="text-portfolio">New York - NY</h3>
                 </div>
             </div>
             <div className="card-portfolio">
                 <img src="poole.png" alt="Imagem de Poole na UK" />
                 <div className="container-text-portfolio">
-                    <img src="icon-map.png" alt="ícone de Localização" />
+                    <img className="icon-map" src="icon-map.png" alt="ícone de Localização" />
                     <h3 className="text-portfolio">Poole - UK</h3>
                 </div>
             </div>
             <div className="card-portfolio">
                 <img src="minas-gerais.png" alt="Imagem de Minas Gerais" />
                 <div className="container-text-portfolio">
-                    <img src="icon-map.png" alt="ícone de Localização" />
+                    <img className="icon-map" src="icon-map.png" alt="ícone de Localização" />
                     <h3 className="text-portfolio">Minas Gerais - BH</h3>
                 </div>
             </div>
